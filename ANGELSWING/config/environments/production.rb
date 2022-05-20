f004b5b761eb6b488688eb3e2291272b569c80ab
rlfalsgh95 <rlfalsgh96@gmail.com>
2022-05-20 23:32:40 +0000

    config default url, add auth routes

--- ANGELSWING/config/environments/production.rb
+++ ANGELSWING/config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # for default url	
+  config.action_controller.default_url_options = { host: '/api/v1' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- ANGELSWING/config/routes.rb
+++ ANGELSWING/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resource :users, only: [:create]
+	post "/auth/signin", to: "users#login"	# used for login
+	get "/auth/auto_login", to: "users#auto_login"	# Use to verify that the user is logged in using JWT. 
 end
