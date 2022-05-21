1604091b6586324bc309efb1872c90f9ef519a6e
rlfalsgh95 <rlfalsgh96@gmail.com>
2022-05-21 00:17:04 +0000

    generate user model, controller

--- ANGELSWING/db/seeds.rb
+++ ANGELSWING/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(first_name: "minho", last_name: "Gil", email: "rlfalsgh95@naver.com", password: "pwd", country: "seoul")
