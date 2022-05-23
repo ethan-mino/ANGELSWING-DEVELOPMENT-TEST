2d47a153da4dfc752a15a8d24fcbad862b19ecc3
rlfalsgh95 <rlfalsgh96@gmail.com>
2022-05-23 15:30:29 +0000

    Add Error Code

--- ANGELSWING/app/utils/ApiResponse.rb
+++ ANGELSWING/app/utils/ApiResponse.rb
@@ -3,11 +3,15 @@ class ApiResponse
 		"INFO-200" => "Successfully processed.",
 		"INFO-210" => "No Data.",
 		
-		"ERROR-000" => "Email already exists.",
 		"ERROR-010" => "Email or password is not valid.",
 		
 		"ERROR-300" => "Required parameter is missing. Please See the Request document.",
 		"ERROR-310" => "Value is invalid. Please See the Request document.",
+		
+		"ERROR-400" => "Duplicate entry",
+		"ERROR-410" => "You do not have permission.",
+		"ERROR-420" => "Data does not exist.",
+		"ERROR-430" => "DB ERROR.",
 		
 		"ERROR-500" => "Internal Server Error. Please contact the customer center."
 	}
