53887c8a99e1e395827a7acd89e504e3f945f326
rlfalsgh95 <rlfalsgh96@gmail.com>
2022-05-23 17:05:40 +0000

    Add content, user constraint

--- ANGELSWING/app/models/content.rb
+++ ANGELSWING/app/models/content.rb
@@ -1,4 +1,6 @@
 class Content < ApplicationRecord
-  belongs_to :user
-  belongs_to :project
+	belongs_to :user
+	belongs_to :project
+	
+	TYPE = "content"
 end
--- ANGELSWING/app/models/user.rb
+++ ANGELSWING/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_many :project
 	def User.username(first_name, last_name)
 		last_name + " " + first_name
 	end
